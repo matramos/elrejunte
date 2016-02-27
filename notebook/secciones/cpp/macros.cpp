8b1fbceb790791389431339615b110c3b0bedb78
Nicolás Guimar <njguimar@gmail.com>
2016-02-27 12:16:12 -0300

    added some constants and macros

--- notebook/secciones/cpp/macros.cpp
+++ notebook/secciones/cpp/macros.cpp
@@ -1,2 +1,18 @@
+//buffers
 #define MEMZ(mem) memset(mem, 0, sizeof mem) // memset => <cstring / string.h >
 #define MEMX(mem,x) memset(mem, (x), sizeof mem)
+
+//loops
+#define forall(i,a,b) for(int (i)=(a);(i)<(b);++(i))
+#define foreach(i,v) for(typeof((v).begin()) (i) = (v).begin(); (i) != (v).end(); ++(i))
+
+//ḿap/pairs
+#define mp make_pair
+#define fi first
+#define se second
+
+//vector
+#define pb push_back
+
+//queries
+#define in(a,v) ((v).find((a))!=(v).end()) // <algorithm>
--- notebook/secciones/tablas_const/constantes.cpp
+++ notebook/secciones/tablas_const/constantes.cpp
@@ -1,2 +1,3 @@
 #define INF 1000000000 // 1 billon, entra en int
 #define EPS 1e-12
+#define PI 3.1415926535897932384626
