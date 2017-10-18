d2efb8aed3d0460121b9486bf726f770e3bb981b
mramos <mramos@frsf.utn.edu.ar>
2017-10-18 19:14:34 -0300

    Se agrego utils y soluciones al solucionario

---
+++ notebook/secciones/utils/string_to_num.cpp
@@ -0,0 +1,11 @@
+#include <sstream>
+string num_to_str(int x){
+  ostringstream convert;
+  return (convert << entero).str();
+}
+
+int str_to_num(string x){
+  int ret;
+  istringstream (x) >> ret;
+  return ret;
+}
