dce8e83f1ae9878b93a259a8f0c688c5d83ff154
Santiago Re <lsantire@hotmail.com>
2017-11-01 20:16:01 -0300

    Se modificó forma en la que estaba min cut
    Quité el .txt; agregué el .cpp y agregué con que problema se probó

---
+++ notebook/secciones/flow/min_cut.cpp
@@ -0,0 +1,27 @@
+//Suponemos un grafo con el formato definido en Edmond Karp o Push relabel
+bitset<MAX_V> type,used; //reset this
+void dfs1(int nodo)
+{
+	type.set(nodo);
+	forall(it,G[nodo]) if(!type[it->fst] && it->snd>0) dfs1(it->fst);
+}
+void dfs2(int nodo)
+{
+	used.set(nodo);
+	forall(it,G[nodo])
+	{
+		if(!type[it->fst])
+		{
+			//edge nodo -> (it->fst) pertenece al min_cut
+			//y su peso original era: it->snd + G[it->fst][nodo]
+			//si no existia arista original al revés
+		}
+		else if(!used[it->fst]) dfs2(it->fst);
+	}
+}
+void minCut() //antes correr algún maxflow()
+{
+	dfs1(SRC);
+	dfs2(SRC);
+	return;
+}
