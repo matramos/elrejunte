9972b7c439c5e412a3e15fdc846c6d25e895f60b
Matías Ramos <matramos@users.noreply.github.com>
2019-10-31 14:41:37 -0300

    Se agrega struc necesaria ya que no se entendia solo

--- notebook/secciones/grafos/kruskal.cpp
+++ notebook/secciones/grafos/kruskal.cpp
@@ -1,6 +1,16 @@
-// Minimun Spanning Tree in O(e log e)
+struct UF{
+    void init(int n){}
+    void unir(int a, int v){}
+    int comp(int n){return 0;}
+}uf;
+vector<ii> G[MAXN];
+int n;
+
+struct Ar{int a,b,w;};
 bool operator<(const Ar& a, const Ar &b){return a.w<b.w;}
 vector<Ar> E;
+
+// Minimun Spanning Tree in O(e log e)
 ll kruskal(){
     ll cost=0;
     sort(E.begin(), E.end());//ordenar aristas de menor a mayor
