1f7cee97da825b57f873011574a92b681704e05e
Unknown <lsantire@hotmail.com>
2020-05-08 23:36:05 -0300

    Pequenio cambio en hash.cpp
    Se agregan comentarios y modifican pequenias cuestiones en hash.cpp para que se visualice mejor en el pdf

--- notebook/secciones/strings/hash.cpp
+++ notebook/secciones/strings/hash.cpp
@@ -1,12 +1,17 @@
 struct Hash {
+	//P must be a prime number, could be randomly generated,
+	//sometimes is good to make it close to alphabet size
 	int P=1777771, MOD[2], PI[2];
 	vector<int> h[2], pi[2];
-	vector<ll> vp[2];
+	vector<ll> vp[2]; //Only used if getChanged is used (delete it if not)
 	Hash(string& s) {
+		//MOD[i] must be a prime of this order, could be randomly generated
 		MOD[0]=999727999; MOD[1]=1070777777;
+		//PI[i] = P^-1 % MOD[i]
 		PI[0]=325255434; PI[1]=10018302;
-		forn(k,2) h[k].resize(s.size()+1), pi[k].resize(s.size()+1), vp[k].rsz(s.size()+1);
-		forn(k,2) {
+		forn(k, 2)
+			h[k].rsz(s.size()+1), pi[k].rsz(s.size()+1), vp[k].rsz(s.size()+1);
+		forn(k, 2) {
 			h[k][0] = 0; pi[k][0] = vp[k][0] = 1;
 			ll p=1;
 			forr(i, 1, s.size()+1) {
@@ -18,7 +23,7 @@ struct Hash {
 	}
 	ll get(int s, int e) { // get hash value of the substring [s, e)
 		ll H[2];
-		forn(i,2) {
+		forn(i, 2) {
 			H[i] = (h[i][e] - h[i][s] + MOD[i]) % MOD[i];
 			H[i] = (1LL * H[i] * pi[i][s]) % MOD[i];
 		}
@@ -31,7 +36,8 @@ struct Hash {
         ll hv = get(s,e), h[2];
         h[1] = hv & ((1LL<<32)-1);
         h[0] = hv >> 32;
-        forn(i,2) h[i] = (h[i] + vp[i][pos] * (val - origVal + MOD[i])) % MOD[i];
+        forn(i, 2)
+        	h[i] = (h[i] + vp[i][pos] * (val - origVal + MOD[i])) % MOD[i];
         return (h[0]<<32)|h[1];
     }
 };
