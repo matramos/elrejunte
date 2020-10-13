963763dc24343b9084514271b2231374822a44c6
lsantire <lsantire2@gmail.com>
2020-10-12 23:12:30 -0300

    Added small changes in the template

--- notebook/secciones/template/template.cpp
+++ notebook/secciones/template/template.cpp
@@ -1,34 +1,34 @@
 #include <bits/stdc++.h>
-#define sqr(a) ((a)*(a))
-#define rsz resize
 #define forr(i,a,b) for(int i=(a);i<(b);i++)
 #define forn(i,n) forr(i,0,n)
 #define dforn(i,n) for(int i=n-1;i>=0;i--)
 #define forall(it,v) for(auto it=v.begin();it!=v.end();it++)
-#define foreach(i, v) for(auto i:v)
 #define sz(c) ((int)c.size())
-#define zero(v) memset(v, 0, sizeof(v))
+#define rsz resize
 #define pb push_back
 #define mp make_pair
 #define lb lower_bound
 #define ub upper_bound
 #define fst first
 #define snd second
-#define PI 3.1415926535897932384626
+
+#ifdef ANARAP
+//local
+#else
+//judge
+#endif
 
 using namespace std;
 
 typedef long long ll;
 typedef pair<int,int> ii;
-typedef vector<int> vi;
-typedef vector<ii> vii;
 
 int main()
 {
-	// agregar g++ -DREJUNTE en compilacion
-	#ifdef REJUNTE
+	// agregar g++ -DANARAP en compilacion
+	#ifdef ANARAP
 		freopen("input", "r", stdin);
-		// freopen("output","w", stdout);
+		//freopen("output","w", stdout);
 	#endif
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
