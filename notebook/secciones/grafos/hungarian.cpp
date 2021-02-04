b1dfe1cf5c5d9cc9b1873b62603092b114469c2f
Luis Santiago Re <lsantire2@gmail.com>
2021-02-04 01:00:43 -0300

    Replaced hungarian implementation and added test
    Signed-off-by: Luis Santiago Re <lsantire2@gmail.com>

--- notebook/secciones/grafos/hungarian.cpp
+++ notebook/secciones/grafos/hungarian.cpp
@@ -1,56 +1,40 @@
-//Dado un grafo bipartito completo con costos no negativos, encuentra el matching perfecto de minimo costo.
-#define tipo double
-tipo cost[N][N], lx[N], ly[N], slack[N]; //llenar: cost=matriz de adyacencia
-int n, max_match, xy[N], yx[N], slackx[N],prev2[N];//n=cantidad de nodos
-bool S[N], T[N]; //sets S and T in algorithm
-void add_to_tree(int x, int prevx) {
-	S[x] = true, prev2[x] = prevx; 
-	forn(y, n) if (lx[x] + ly[y] - cost[x][y] < slack[y] - EPS)
-		slack[y] = lx[x] + ly[y] - cost[x][y], slackx[y] = x;
-}
-void update_labels(){
-	tipo delta = INF; 
-	forn (y, n) if (!T[y]) delta = min(delta, slack[y]);
-	forn (x, n) if (S[x]) lx[x] -= delta;
-	forn (y, n) if (T[y]) ly[y] += delta; else slack[y] -= delta;
-}
-void init_labels(){
-	zero(lx), zero(ly);
-	forn (x,n) forn(y,n) lx[x] = max(lx[x], cost[x][y]);
-}
-void augment() {
-	if (max_match == n) return; 
-	int x, y, root, q[N], wr = 0, rd = 0; 
-	memset(S, false, sizeof(S)), memset(T, false, sizeof(T)); 
-	memset(prev2, -1, sizeof(prev2)); 
-	forn (x, n) if (xy[x] == -1){
-		q[wr++] = root = x, prev2[x] = -2;
-		S[x] = true; break; }
-	forn (y, n) slack[y] = lx[root] + ly[y] - cost[root][y], slackx[y] = root;
-	while (true){
-		while (rd < wr){
-			x = q[rd++];
-			for (y = 0; y < n; y++) if (cost[x][y] == lx[x] + ly[y] && !T[y]){
-				if (yx[y] == -1) break; T[y] = true; 
-				q[wr++] = yx[y], add_to_tree(yx[y], x); }
-			if (y < n) break; }
-		if (y < n) break; 
-		update_labels(), wr = rd = 0; 
-		for (y = 0; y < n; y++) if (!T[y] && slack[y] == 0){
-			if (yx[y] == -1){x = slackx[y]; break;}
-			else{
-				T[y] = true; 
-				if (!S[yx[y]]) q[wr++] = yx[y], add_to_tree(yx[y], slackx[y]);
-			}}
-		if (y < n) break; }
-	if (y < n){
-		max_match++; 
-		for (int cx = x, cy = y, ty; cx != -2; cx = prev2[cx], cy = ty)
-			ty = xy[cx], yx[cy] = cx, xy[cx] = cy;
-		augment(); }
-}
-tipo hungarian(){
-	tipo ret = 0; max_match = 0, memset(xy, -1, sizeof(xy)); 
-	memset(yx, -1, sizeof(yx)), init_labels(), augment(); //steps 1-3
-	forn (x,n) ret += cost[x][xy[x]]; return ret;
-}
+typedef long double td; typedef vector<int> vi; typedef vector<td> vd;
+const td INF=1e100;//for maximum set INF to 0, and negate costs
+bool zero(td x){return fabs(x)<1e-9;}//change to x==0, for ints/ll
+struct Hungarian{
+    int n; vector<vd> cs; vi L, R;
+    Hungarian(int N, int M):n(max(N,M)),cs(n,vd(n)),L(n),R(n){
+        forn(x,N)forn(y,M)cs[x][y]=INF;
+    }
+    void set(int x,int y,td c){cs[x][y]=c;}
+	td assign() {
+		int mat = 0; vd ds(n), u(n), v(n); vi dad(n), sn(n);
+		forn(i,n)u[i]=*min_element(ALL(cs[i]));
+		forn(j,n){v[j]=cs[0][j]-u[0];forr(i,1,n)v[j]=min(v[j],cs[i][j]-u[i]);}
+		L=R=vi(n, -1);
+		forn(i,n)forn(j,n)
+			if(R[j]==-1&&zero(cs[i][j]-u[i]-v[j])){L[i]=j;R[j]=i;mat++;break;}
+		for(;mat<n;mat++){
+		    int s=0, j=0, i;
+		    while(L[s] != -1)s++;
+		    fill(ALL(dad),-1);fill(ALL(sn),0);
+		    forn(k,n)ds[k]=cs[s][k]-u[s]-v[k];
+		    for(;;){
+		        j = -1;
+		        forn(k,n)if(!sn[k]&&(j==-1||ds[k]<ds[j]))j=k;
+		        sn[j] = 1; i = R[j];
+		        if(i == -1) break;
+		        forn(k,n)if(!sn[k]){
+		            auto new_ds=ds[j]+cs[i][k]-u[i]-v[k];
+		            if(ds[k] > new_ds){ds[k]=new_ds;dad[k]=j;}
+		        }
+		    }
+		    forn(k,n)if(k!=j&&sn[k]){auto w=ds[k]-ds[j];v[k]+=w,u[R[k]]-=w;}
+		    u[s] += ds[j];
+		    while(dad[j]>=0){int d = dad[j];R[j]=R[d];L[R[j]]=j;j=d;}
+		    R[j]=s;L[s]=j;
+		}
+		td value=0;forn(i,n)value+=cs[i][L[i]];
+		return value;
+	}
+};
---
+++ notebook/secciones/grafos/hungarian_test.cpp
@@ -0,0 +1,106 @@
+//Problem: https://codeforces.com/gym/101635/problem/G
+#include <bits/stdc++.h>
+#define forr(i,a,b) for(int i=(a);i<(b);i++)
+#define fore(i,a,b) for(int i=(a);i<(b);i++)
+#define forn(i,n) forr(i,0,n)
+#define dforn(i,n) for(int i=n-1;i>=0;i--)
+#define forall(it,v) for(auto it=v.begin();it!=v.end();it++)
+#define sz(c) ((int)c.size())
+#define rsz resize
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define fst first
+#define snd second
+#define ALL(x) x.begin(), x.end()
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> ii;
+
+typedef int td; typedef vector<int> vi; typedef vector<td> vd;
+const td INF=1e9;//for maximum set INF to 0, and negate costs
+bool zero(td x){return x==0;}//change to x==0, for ints/ll
+struct Hungarian{
+    int n; vector<vd> cs; vi L, R;
+    Hungarian(int N, int M):n(max(N,M)),cs(n,vd(n)),L(n),R(n){
+        forn(x,N)forn(y,M)cs[x][y]=INF;
+    }
+    void set(int x,int y,td c){cs[x][y]=c;}
+	td assign() {
+		int mat = 0; vd ds(n), u(n), v(n); vi dad(n), sn(n);
+		forn(i,n)u[i]=*min_element(ALL(cs[i]));
+		forn(j,n){v[j]=cs[0][j]-u[0];forr(i,1,n)v[j]=min(v[j],cs[i][j]-u[i]);}
+		L=R=vi(n, -1);
+		forn(i,n)forn(j,n)
+			if(R[j]==-1&&zero(cs[i][j]-u[i]-v[j])){L[i]=j;R[j]=i;mat++;break;}
+		for(;mat<n;mat++){
+		    int s=0, j=0, i;
+		    while(L[s] != -1)s++;
+		    fill(ALL(dad),-1);fill(ALL(sn),0);
+		    forn(k,n)ds[k]=cs[s][k]-u[s]-v[k];
+		    for(;;){
+		        j = -1;
+		        forn(k,n)if(!sn[k]&&(j==-1||ds[k]<ds[j]))j=k;
+		        sn[j] = 1; i = R[j];
+		        if(i == -1) break;
+		        forn(k,n)if(!sn[k]){
+		            auto new_ds=ds[j]+cs[i][k]-u[i]-v[k];
+		            if(ds[k] > new_ds){ds[k]=new_ds;dad[k]=j;}
+		        }
+		    }
+		    forn(k,n)if(k!=j&&sn[k]){auto w=ds[k]-ds[j];v[k]+=w,u[R[k]]-=w;}
+		    u[s] += ds[j];
+		    while(dad[j]>=0){int d = dad[j];R[j]=R[d];L[R[j]]=j;j=d;}
+		    R[j]=s;L[s]=j;
+		}
+		td value=0;forn(i,n)value+=cs[i][L[i]];
+		return value;
+	}
+};
+
+
+
+int main(){
+	#ifdef ANARAP
+		freopen("input.in", "r",stdin);
+	#endif
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int nn,mm;
+	cin >> nn >> mm;
+	vector<ii> bots(nn), devs(mm);
+	forn(i,nn) cin >> bots[i].fst >> bots[i].snd;
+	forn(i,mm) cin >> devs[i].fst >> devs[i].snd;
+	int x,y;
+	cin >> x >> y;
+	int left = mm+nn-1, right = nn;
+	Hungarian h(left, right);
+	forn(i,mm)
+	{
+		forn(j,nn)
+		{
+			int d = abs(devs[i].fst - bots[j].fst) + abs(devs[i].snd - bots[j].snd);
+			d += abs(x - bots[j].fst) + abs(y - bots[j].snd);
+			//cost[i][mm+j] = d;
+			h.set(i,j,d);
+		}
+	}
+	forn(i,nn-1)
+	{
+		forn(j,nn)
+		{
+			int d = 2*(abs(x - bots[j].fst) + abs(y - bots[j].snd));
+			//cost[mm+nn+i][mm+j] = d;
+			h.set(mm+i,j,d);
+		}
+	}
+	assert(left >= right);
+	cout << h.assign() << '\n';
+	return 0;
+	
+}
+
