05979449e018bcbfaeb9bfa0882d0f0d3b0234f8
FedeNQ <fedenahuel07@hotmail.com>
2021-11-11 18:37:43 -0300

    Agrego comentario en dyn convexhull

--- notebook/secciones/geometria/convex_hull_dyn.cpp
+++ notebook/secciones/geometria/convex_hull_dyn.cpp
@@ -1,15 +1,16 @@
 // Superior
 struct Semi_Chull {
-    set<pto> pts;
+    set<pto> pts; // this set maintains the convex hull without collinears points
+    //in case that we want to have them on the set, make the changes commented below
     bool check(pto p) {
         if(pts.empty()) return false;
         if(*pts.rbegin() < p) return false;
         if(p < *pts.begin()) return false;
         auto it = pts.lower_bound(p);
-        if(it->x == p.x) return p.y <= it->y;
+        if(it->x == p.x) return p.y <= it->y; //ignore it to take in count collinears points too
         pto b = *it;
         pto a = *prev(it);
-        return ((b-p)^(a-p))+EPS >= 0;
+        return ((b-p)^(a-p))+EPS >= 0; //> 0 to take in count collinears points too
     }
  
     void add(pto p) {
@@ -21,7 +22,7 @@ struct Semi_Chull {
             if(next(it) == pts.end() || next(next(it)) == pts.end()) break;
             pto a = *next(it);
             pto b = *next(next(it));
-            if(((b-a)^(p-a))+EPS >= 0) {
+            if(((b-a)^(p-a))+EPS >= 0) { //> 0 to take in count collinears points too
                 pts.erase(next(it));
             } else break;
         }
@@ -32,7 +33,7 @@ struct Semi_Chull {
             if(it == pts.begin() || prev(it) == pts.begin()) break;
             pto a = *prev(it);
             pto b = *prev(prev(it));
-            if(((b-a)^(p-a))-EPS <=0) {
+            if(((b-a)^(p-a))-EPS <=0) { //< 0 to take in count collinears points too
                 pts.erase(prev(it));
             } else break;
         }
