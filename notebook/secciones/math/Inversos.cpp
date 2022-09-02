398c0531d46fd103a7a1aa9b0efa200251bbb614
FedeNQ <fedenahuel07@hotmail.com>
2022-09-02 16:57:04 -0300

    find inv O(N)

--- notebook/secciones/math/Inversos.cpp
+++ notebook/secciones/math/Inversos.cpp
@@ -10,3 +10,19 @@ int inverso(int x) //O(log x)
 	return expMod(x, eulerPhi(MOD)-1);//si mod no es primo(sacar a mano)
 	return expMod(x, MOD-2);//si mod es primo
 }
+
+
+void pre(){ //o(MAXMOD)
+	// pay attention on MAXMOD value
+	fact[0] = 1;
+	forr(i,1,MAXMOD-2) fact[i] = (fact[i-1]*i)%MOD;
+	invFact[MAXMOD-3] = inverso(fact[MAXMOD-3]);
+	dforn(i, MAXMOD-3) invFact[i] = (invFact[i+1]*(i+1))%MOD;
+	return;
+}
+ 
+// n! / k!*(n-k)!
+ll comb(ll nn, ll k){
+	if (k < 0 || nn < k) return 0;
+	return (fact[nn]*invFact[k]%MOD)*invFact[nn-k]%MOD;
+}
