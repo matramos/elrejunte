a3ddd9599a1b61b503991e8604bb56704d34191b
Luis Santiago Re <lsantire2@gmail.com>
2023-03-10 20:11:24 +0000

    Delete not used files in 'cpp' section

--- notebook/secciones/cpp/fast_cpp_io.cpp
+++
@@ -1,1 +0,0 @@
-ios_base::sync_with_stdio(false); cin.tie(NULL);
--- notebook/secciones/cpp/typedef.cpp
+++
@@ -1,6 +0,0 @@
-typedef long long ll;
-typedef long double ld;
-typedef pair<int,int> pii;
-typedef vector<pii> vpii;
-typedef vector<int> vi;
-typedef vector<vi> vvi;
