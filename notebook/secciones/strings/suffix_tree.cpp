c8b8f6647f7e3a16fde75a88bd64224a346538b9
Luis Santiago Re <lsantire2@gmail.com>
2023-03-14 23:45:53 +0000

    Make some small changes in SuffixTree

--- notebook/secciones/strings/suffix_tree.cpp
+++ notebook/secciones/strings/suffix_tree.cpp
@@ -1,20 +1,28 @@
+const int INF = 1000100100;
 //The SuffixTree of S is the compressed trie that would result
 //after inserting every suffix of S.
 //As it is a COMPRESSED trie, some edges may correspond to strings, instead
 //of chars, and the compression is done in a way that every vertex that
 //doesn't correspond to a suffix and has only one descendent, is omitted.
 struct SuffixTree {
-	char s[MAXN];
-	map<int,int> to[2*MAXN];//key is fst char of substring on edge to value
+	vector<char> s;
+	vector<map<int,int>> to;//key is fst char of substring on edge to value
 	//s[fpos[i], fpos[i]+len[i]) is the substring on the edge between
 	//i's father and i.
 	//link[i] goes to the node that corresponds to the substring that result
 	//after "removing" the first character of the substring that i represents
 	//note that link is only defined for every internal (non-leaf) node.
-	int len[2*MAXN] = {INF}, fpos[2*MAXN], link[2*MAXN];
-	int node = 0, pos = 0, sz = 1, n = 0;
+	vector<int> len, fpos, link;
+	SuffixTree(int nn = 0) { // O(nn). nn should be the expected size
+		s.reserve(nn); to.reserve(2*nn); len.reserve(2*nn);
+		fpos.reserve(2*nn); link.reserve(2*nn);
+		make_node(0, INF);
+	}
+	int node = 0, pos = 0, n = 0;
 	int make_node(int p, int l) {
-		fpos[sz] = p; len[sz] = l; return sz++;}
+		fpos.pb(p); len.pb(l); to.pb({}); link.pb(0);
+		return sz(to)-1;
+	}
 	void go_edge() {
 		while(pos > len[to[node][s[n-pos]]]) {
 			node = to[node][s[n-pos]];
@@ -22,7 +30,7 @@ struct SuffixTree {
 		}
 	}
 	void add(char c) {
-		s[n++] = c; pos++;
+		s.pb(c); n++; pos++;
 		int last = 0;
 		while(pos > 0) {
 			go_edge();
@@ -44,5 +52,12 @@ struct SuffixTree {
 			if(node == 0) pos--;
 			else node = link[node];
 		}
+	}
+	// Call this after you finished building the SuffixTree to correctly
+	// set some values of the vector 'len' that currently have a big
+	// value (because of INF usage). Note that you shouldn't call 'add'
+	// anymore after calling this method.
+	void finishedAdding() {
+		forn(i, sz(len)) if(len[i] + fpos[i] > n) len[i] = n - fpos[i];
 	}
 };
--- notebook/secciones/strings/suffix_tree_test.cpp
+++ notebook/secciones/strings/suffix_tree_test.cpp
@@ -15,8 +15,6 @@
 #define fst first
 #define snd second
 #define PI 3.1415926535897932384626
-#define MAXN 200010
-#define INF 1000100
 
 using namespace std;
 
@@ -25,23 +23,31 @@ typedef pair<int,int> ii;
 typedef vector<int> vi;
 typedef vector<ii> vii;
 
+const int INF = 1000100100;
 //The SuffixTree of S is the compressed trie that would result
 //after inserting every suffix of S.
 //As it is a COMPRESSED trie, some edges may correspond to strings, instead
 //of chars, and the compression is done in a way that every vertex that
 //doesn't correspond to a suffix and has only one descendent, is omitted.
 struct SuffixTree {
-	char s[MAXN];
-	map<int,int> to[2*MAXN];//key is fst char of substring on edge to value
+	vector<char> s;
+	vector<map<int,int>> to;//key is fst char of substring on edge to value
 	//s[fpos[i], fpos[i]+len[i]) is the substring on the edge between
 	//i's father and i.
 	//link[i] goes to the node that corresponds to the substring that result
 	//after "removing" the first character of the substring that i represents
 	//note that link is only defined for every internal (non-leaf) node.
-	int len[2*MAXN] = {INF}, fpos[2*MAXN], link[2*MAXN];
-	int node = 0, pos = 0, sz = 1, n = 0;
+	vector<int> len, fpos, link;
+	SuffixTree(int nn = 0) { // O(nn). nn should be the expected size
+		s.reserve(nn); to.reserve(2*nn); len.reserve(2*nn);
+		fpos.reserve(2*nn); link.reserve(2*nn);
+		make_node(0, INF);
+	}
+	int node = 0, pos = 0, n = 0;
 	int make_node(int p, int l) {
-		fpos[sz] = p; len[sz] = l; return sz++;}
+		fpos.pb(p); len.pb(l); to.pb({}); link.pb(0);
+		return sz(to)-1;
+	}
 	void go_edge() {
 		while(pos > len[to[node][s[n-pos]]]) {
 			node = to[node][s[n-pos]];
@@ -49,7 +55,7 @@ struct SuffixTree {
 		}
 	}
 	void add(char c) {
-		s[n++] = c; pos++;
+		s.pb(c); n++; pos++;
 		int last = 0;
 		while(pos > 0) {
 			go_edge();
@@ -72,23 +78,31 @@ struct SuffixTree {
 			else node = link[node];
 		}
 	}
+	// Call this after you finished building the SuffixTree to correctly
+	// set some values of the vector 'len' that currently have a big
+	// value (because of INF usage). Note that you shouldn't call 'add'
+	// anymore after calling this method.
+	void finishedAdding() {
+		forn(i, sz(len)) if(len[i] + fpos[i] > n) len[i] = n - fpos[i];
+	}
 };
 
 int main()
 {
-	#ifdef REJUNTE
+	#ifdef ANARAP
 	freopen("input.in","r",stdin);
 	//freopen("output.out","w",stdout);
 	#endif
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	SuffixTree st;
 	string s;
 	int q;
 	cin >> s >> q;
 	s.push_back('$');
-	forn(i,s.size()) st.add(s[i]);
+	SuffixTree st(sz(s));
+	forn(i,sz(s)) st.add(s[i]);
+	st.finishedAdding();
 	/*forn(i,st.sz)
 	{
 		cout << i << ' ' << st.fpos[i];
@@ -104,7 +118,7 @@ int main()
 		string x;
 		cin >> x;
 		int pos = 0, ans = 0;
-		while(pos < x.size())
+		while(pos < sz(x))
 		{
 			if(st.to[0].count(x[pos]) == 0)
 			{
@@ -114,18 +128,18 @@ int main()
 			ans++;
 			int node = 0;
 			bool ok = true;
-			while(ok && pos < x.size() && st.to[node].count(x[pos]))
+			while(ok && pos < sz(x) && st.to[node].count(x[pos]))
 			{
 				int nextNode = st.to[node][x[pos]];
 				forn(i,st.len[nextNode])
 				{
-					if(pos < x.size() && x[pos] == s[st.fpos[nextNode]+i]) pos++;
+					if(pos < sz(x) && x[pos] == s[st.fpos[nextNode]+i]) pos++;
 					else
 					{
 						ok = false;
 						break;
 					}
-					if(pos == x.size()) break;
+					if(pos == sz(x)) break;
 				}
 				node = nextNode;
 			}
