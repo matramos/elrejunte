c17301def36ba28de8391ab6ca7494193e4cbb61
Luis Santiago Re <lsantire2@gmail.com>
2023-04-02 03:14:38 +0100

    Update hopcroft karp and test

--- notebook/secciones/flow/hopcroft_karp.cpp
+++ notebook/secciones/flow/hopcroft_karp.cpp
@@ -1,33 +1,43 @@
-vector<int> g[MAXN]; // [0,n)->[0,m)
-int n,m;
-int mt[MAXN],mt2[MAXN],ds[MAXN];
-bool bfs(){
-	queue<int> q;
-	memset(ds,-1,sizeof(ds));
-	forn(i,n)if(mt2[i]<0)ds[i]=0,q.push(i);
-	bool r=false;
-	while(!q.empty()){
-		int x=q.front();q.pop();
-		for(int y:g[x]){
-			if(mt[y]>=0&&ds[mt[y]]<0)ds[mt[y]]=ds[x]+1,q.push(mt[y]);
-			else if(mt[y]<0)r=true;
+struct HopcroftKarp {
+	// [0,n)->[0,m) (ids independent in each side)
+	vector<vector<int>> g;
+	int n, m;
+	vector<int> mt, mt2, ds;
+	HopcroftKarp(int nn, int mm) { n = nn; m = mm; g.rsz(n); }
+	void add(int a, int b) { g[a].pb(b); }
+	bool bfs(){
+		queue<int> q;
+		ds = vector<int>(n, -1);
+		forn(i,n) if(mt2[i] < 0) ds[i] = 0, q.push(i);
+		bool r = false;
+		while(!q.empty()) {
+			int x = q.front(); q.pop();
+			for(int y : g[x]) {
+				if(mt[y] >= 0 && ds[mt[y]] < 0) {
+					ds[mt[y]] = ds[x] + 1, q.push(mt[y]);
+				}
+				else if(mt[y] < 0) r = true;
+			}
 		}
+		return r;
 	}
-	return r;
-}
-bool dfs(int x){
-	for(int y:g[x])if(mt[y]<0||ds[mt[y]]==ds[x]+1&&dfs(mt[y])){
-		mt[y]=x;mt2[x]=y;
-		return true;
+	bool dfs(int x){
+		for(int y : g[x]) {
+			if(mt[y] < 0 || ds[mt[y]] == ds[x] + 1 && dfs(mt[y])) {
+				mt[y] = x; mt2[x] = y;
+				return true;
+			}
+		}
+		ds[x] = 1<<30;
+		return false;
 	}
-	ds[x]=1<<30;
-	return false;
-}
-int mm(){ //O(sqrt(V)*E)
-	int r=0;
-	memset(mt,-1,sizeof(mt));memset(mt2,-1,sizeof(mt2));
-	while(bfs()){
-		forn(i,n)if(mt2[i]<0)r+=dfs(i);
+	int mm() { //O(sqrt(V)*E)
+		int r = 0;
+		mt = vector<int>(m, -1);
+		mt2 = vector<int>(n, -1);
+		while(bfs()){
+			forn(i,n) if(mt2[i] < 0) r += dfs(i);
+		}
+		return r;
 	}
-	return r;
-}
+};
--- notebook/secciones/flow/hopcroft_karp_test.cpp
+++ notebook/secciones/flow/hopcroft_karp_test.cpp
@@ -1,4 +1,4 @@
-//Problem: https://codeforces.com/contest/1423/problem/B
+// https://codeforces.com/gym/104252/problem/F
 #include <bits/stdc++.h>
 #define forr(i,a,b) for(int i=(a);i<(b);i++)
 #define forn(i,n) forr(i,0,n)
@@ -14,100 +14,132 @@
 #define snd second
 
 #ifdef ANARAP
-//prueba
+//local
 #else
-//real
+//judge
 #endif
 
 using namespace std;
 
 typedef long long ll;
 typedef pair<int,int> ii;
-deque<pair<int,ii>> agregar;
 
-
-
-#define MAXN 10003
-
-vector<int> g[MAXN]; // [0,n)->[0,m)
-int n,m;
-int mt[MAXN],mt2[MAXN],ds[MAXN];
-bool bfs(){
-	queue<int> q;
-	memset(ds,-1,sizeof(ds));
-	forn(i,n)if(mt2[i]<0)ds[i]=0,q.push(i);
-	bool r=false;
-	while(!q.empty()){
-		int x=q.front();q.pop();
-		for(int y:g[x]){
-			if(mt[y]>=0&&ds[mt[y]]<0)ds[mt[y]]=ds[x]+1,q.push(mt[y]);
-			else if(mt[y]<0)r=true;
+struct HopcroftKarp {
+	// [0,n)->[0,m) (ids independent in each side)
+	vector<vector<int>> g;
+	int n, m;
+	vector<int> mt, mt2, ds;
+	HopcroftKarp(int nn, int mm) { n = nn; m = mm; g.rsz(n); }
+	void add(int a, int b) { g[a].pb(b); }
+	bool bfs(){
+		queue<int> q;
+		ds = vector<int>(n, -1);
+		forn(i,n) if(mt2[i] < 0) ds[i] = 0, q.push(i);
+		bool r = false;
+		while(!q.empty()) {
+			int x = q.front(); q.pop();
+			for(int y : g[x]) {
+				if(mt[y] >= 0 && ds[mt[y]] < 0) {
+					ds[mt[y]] = ds[x] + 1, q.push(mt[y]);
+				}
+				else if(mt[y] < 0) r = true;
+			}
 		}
+		return r;
 	}
-	return r;
-}
-bool dfs(int x){
-	for(int y:g[x])if(mt[y]<0||ds[mt[y]]==ds[x]+1&&dfs(mt[y])){
-		mt[y]=x;mt2[x]=y;
-		return true;
+	bool dfs(int x){
+		for(int y : g[x]) {
+			if(mt[y] < 0 || ds[mt[y]] == ds[x] + 1 && dfs(mt[y])) {
+				mt[y] = x; mt2[x] = y;
+				return true;
+			}
+		}
+		ds[x] = 1<<30;
+		return false;
 	}
-	ds[x]=1<<30;
-	return false;
-}
-int mm(){
-	int r=0;
-	memset(mt,-1,sizeof(mt));memset(mt2,-1,sizeof(mt2));
-	while(bfs()){
-		forn(i,n)if(mt2[i]<0)r+=dfs(i);
+	int mm() { //O(sqrt(V)*E)
+		int r = 0;
+		mt = vector<int>(m, -1);
+		mt2 = vector<int>(n, -1);
+		while(bfs()){
+			forn(i,n) if(mt2[i] < 0) r += dfs(i);
+		}
+		return r;
 	}
-	return r;
+};
+
+const int MAXN = 100;
+
+int dfs(vector<int> g[MAXN], vector<int>& vsz, int node, int p)
+{
+	vsz[node] = 1;
+	forall(it, g[node]) if(*it != p) vsz[node] += dfs(g, vsz, *it, node);
+	return vsz[node];
 }
 
+vector<int> g1[MAXN], g2[MAXN];
+vector<int> vsz1, vsz2;
 
+bool solve(int n1, int p1, int n2, int p2)
+{
+	int n = sz(g1[n1]), m = sz(g2[n2]);
+	bool hasp = n1 != p1;
+	if(m - hasp <= 0) return true; 
+	if(vsz1[n1] < vsz2[n2] || n < m) return false;
+	HopcroftKarp hk(n, m);
+	forn(i,n) if(g1[n1][i] != p1) forn(j,m) if(g2[n2][j] != p2) 
+	{
+		if(solve(g1[n1][i], n1, g2[n2][j], n2)) hk.add(i, j);
+	}
+	return hk.mm() == m - hasp;
+}
 
 int main()
 {
+	// agregar g++ -DANARAP en compilacion
 	#ifdef ANARAP
 		freopen("input.in", "r", stdin);
-		//freopen("output.out", "w", stdout);
-		unsigned t0, t1; t0=clock();
+		//freopen("output","w", stdout);
 	#endif
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
-	
-	cin >> n;	m=n;
-	int a;	cin >> a;
-	forn(i,a){
-		int u,v, d; cin >> u >> v >> d;
-		u--; v--;
-		agregar.pb(mp(d, mp(u, v)));
+	int n1, n2;
+	cin >> n1;
+	forn(_,n1-1)
+	{
+		int a,b;
+		cin >> a >> b;
+		a--;b--;
+		g1[a].pb(b);
+		g1[b].pb(a);
 	}
-	sort(agregar.begin(), agregar.end());
-	
-	auto last = agregar.end();	last--;
-	int L = 0, R = last->fst+2;
-		
-	while(L<R){
-		
-		int M = (L+R)/2;
-		forn(i,n) g[i].clear();
-		forall(it, agregar){
-			if(it->fst <= M) g[it->snd.fst].pb(it->snd.snd);
-			else break;
+	cin >> n2;
+	forn(_,n2-1)
+	{
+		int a,b;
+		cin >> a >> b;
+		a--;b--;
+		g2[a].pb(b);
+		g2[b].pb(a);
+	}
+	if(n1 < n2)
+	{
+		cout << "N\n";
+		return 0;
+	}
+	vsz1.rsz(n1);
+	vsz2.rsz(n2);
+	dfs(g2, vsz2, 0, 0);
+	forn(i,n1)
+	{
+		dfs(g1, vsz1, i, i);
+		if(solve(i, i, 0, 0))
+		{
+			cout << "Y\n";
+			return 0;
 		}
-		
-		if(mm()==n) R = M;
-		else L = M+1;
 	}
-	if(L == last->fst+2) cout << "-1\n";
-	else cout << L << "\n";
-	
-	#ifdef ANARAP
-	t1 = clock();double time = (double(t1-t0)/CLOCKS_PER_SEC); cout << "Execution Time: " << time << endl;
-	#endif
+	cout << "N\n";
 	return 0;
 }
- 
-
-
