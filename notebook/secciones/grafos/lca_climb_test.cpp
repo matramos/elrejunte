0103f795bf3eaa978b9c296253edc8551170d8fb
Luis Santiago Re <lsantire2@gmail.com>
2023-08-07 00:54:16 +0100

    Make lca a struct with dynamic size and add test

--- notebook/secciones/grafos/lca_climb_test.cpp
+++ notebook/secciones/grafos/lca_climb_test.cpp
@@ -1,3 +1,29 @@
+// https://cses.fi/problemset/task/1135
+#include <bits/stdc++.h>
+#define forr(i,a,b) for(int i=(a);i<(b);i++)
+#define forn(i,n) forr(i,0,n)
+#define dforn(i,n) for(int i=n-1;i>=0;i--)
+#define forall(it,v) for(auto it=v.begin();it!=v.end();it++)
+#define sz(c) ((int)c.size())
+#define rsz resize
+#define pb push_back
+#define mp make_pair
+#define lb lower_bound
+#define ub upper_bound
+#define fst first
+#define snd second
+
+#ifdef ANARAP
+//local
+#else
+//judge
+#endif
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int,int> ii;
+
 #define lg(x) (31-__builtin_clz(x))//=floor(log2(x))
 // Usage: 1) Create 2) Add edges 3) Call build 4) Use
 struct LCA {
@@ -38,3 +64,34 @@ struct LCA {
 		return L[a] + L[b] - 2*L[lca(a, b)];
 	}
 };
+
+int main()
+{
+	// agregar g++ -DANARAP en compilacion
+	#ifdef ANARAP
+		freopen("input.in", "r", stdin);
+		//freopen("output","w", stdout);
+	#endif
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n,q;
+	cin >> n >> q;
+	LCA lca(n,0);
+	forn(_,n-1)
+	{
+		int a,b;
+		cin >> a >> b;
+		a--;b--;
+		lca.addEdge(a,b);
+	}
+	lca.build();
+	forn(_,q)
+	{
+		int a,b;
+		cin >> a >> b;
+		a--;b--;
+		cout << lca.dist(a,b) << '\n';
+	}	
+	return 0;
+}
