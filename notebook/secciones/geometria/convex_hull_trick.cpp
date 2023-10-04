6554c9a17912ef620f64ffd5b7f1fe1c32d596e0
JU4N98 <juanpablocabana2@gmail.com>
2023-10-04 19:35:06 -0300

    fixes CHT, adds CHT test

--- notebook/secciones/geometria/convex_hull_trick.cpp
+++ notebook/secciones/geometria/convex_hull_trick.cpp
@@ -1,38 +1,35 @@
 struct CHT{
-	vector<pto> hull, normal;
-	T f; int pos;
-	
-	void init(vector<pto> lines, bool mini){
-		f = mini ? 1 : -1;
-		pos = 0;
-		forn(i,sz(lines)) lines[i] = lines[i]*f;
-		sort(lines.begin(),lines.end()); // if lines aren't sorted
-		forn(i,sz(lines)) add(lines[i]);
-	}
-	void add(pto l) { // y = mx+b => pto(m,b)
-		while(sz(normal) && normal.back()*(l-hull.back())<0) {
-			hull.pop_back();
-			normal.pop_back();
+	deque<pto> h;
+	T f = 1, pos;
+	CHT(bool min_=0): f(min_ ? 1 : -1), pos(0){} // min_=1 for min queries
+	void add(pto p) { // O(1), pto(m,b) <=> y = mx + b 
+		p = p*f;
+		if(h.empty()) { h.pb(p); return; }
+		
+		// p.x should be the lower/greater hull x
+		assert(p.x <= h[0].x || p.x >= h.back().x);
+		if(p.x <= h[0].x) {
+			while(sz(h) > 1 && h[0].left(p,h[1])) h.pop_front(), pos--;
+			h.push_front(p); pos++;
+		}else{
+			while(sz(h) > 1 && h[sz(h)-1].left(h[sz(h)-2], p)) h.pop_back();
+			h.pb(p);
 		}
-		if(sz(hull)){
-			pto n = (l-hull.back());
-			normal.pb({-n.y,n.x});
-		}
-		hull.pb(l);
+		pos = min(max(T(0),pos), T(sz(h)-1));
 	}
-	T get(T x) { // lg(sz(hull))
+	T get(T x){ 
 		pto q = {x,1};
-		int idx = int(lower_bound(normal.begin(),normal.end(),q,[](pto a, pto b){
-			return (a^b)>0;
-		}) - normal.begin());
-		return f*(q*hull[idx]);
-	}
-	vector<T> get(vector<T> q){ // O(1) if queries are sorted
-		vector<T> ret;
-		forn(i,sz(q)){
-			while(pos<sz(normal)-1 && (normal[pos]^pto(q[i],1))>0) pos++;
-			ret.pb(hull[pos]*pto(q[i],1)*f);
+		// O(log) query for unordered x
+		int L = 0,R = sz(h)-1, M;
+		while(L < R) {
+			M = (L+R)/2;
+			if(h[M+1]*q <= h[M]*q) L = M+1;
+			else R = M;
 		}
-		return ret;
+		return h[L]*q*f;
+		// O(1) query for ordered x
+		while(pos > 0 && h[pos-1]*q < h[pos]*q) pos--;
+		while(pos < sz(h)-1 && h[pos+1]*q < h[pos]*q) pos++;
+		return h[pos]*q*f;
 	}
 };
