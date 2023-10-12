9ce6f12bf62caa853e6ec9ade4226f1197d3309f
Luis Santiago Re <lsantire2@gmail.com>
2023-10-12 01:42:26 +0100

    Improve test generator

--- notebook/secciones/utils/test_generator.py
+++ notebook/secciones/utils/test_generator.py
@@ -1,26 +1,27 @@
-import sys, subprocess
-from random import randint
-
-# usage:
+# usage: (note that test_generator.py refers to this file)
 # 1. Modify the code below to generate the tests you want to use
 # 2. Compile the 2 solutions to compare (e.g. A.cpp B.cpp into A B)
-# 3. run: python3 generador.py A B
-# Note that 'generador.py', 'A' and 'B' must be in the SAME FOLDER.
+# 3. run: python3 test_generator.py A B
+# Note that 'test_generator.py', 'A' and 'B' must be in the SAME FOLDER
 # Note that A and B must READ FROM STANDARD INPUT, not from file,
-#	careful with the usual freopen("input.in", "r", stdin) in them
+#	be careful with the usual freopen("input.in", "r", stdin) in them
+import sys, subprocess
+from datetime import datetime
+from random import randint, seed
 
+def buildTestCase(): # example of trivial "a+b" problem
+	a = randint(1,100)
+	b = randint(1,100)
+	return f"{a} {b}\n"
+
+seed(datetime.now().timestamp())
 ntests = 100 # change as wanted
 sol1 = sys.argv[1]
 sol2 = sys.argv[2]
+# Sometimes it's a good idea to use extra arguments that could then be
+# passed to 'buildTestCase' and help you "shape" your tests
 for curtest in range(ntests):
-	test_case = ""
-	# STARTS test creation (change according to problem)
-	# example of trivial "a+b" problem
-	a = randint(1,100)
-	b = randint(1,100)
-	test_case += f"{a} {b}\n"
-	# ENDS test creation
-
+	test_case = buildTestCase()
 	# Here the test is executed and outputs are compared
 	print("running... ", end='')
 	ans1 = subprocess.check_output(f"./{sol1}",
@@ -28,6 +29,7 @@ for curtest in range(ntests):
 	ans2 = subprocess.check_output(f"./{sol2}",
 		input=test_case.encode('utf-8')).decode('utf-8')
 	if ans1 == ans2:
+		assert ans1 != "", 'ERROR?? ans1 = ans2 = empty string ("")'
 		print("OK")
 	else:
 		print("FAILED!")
