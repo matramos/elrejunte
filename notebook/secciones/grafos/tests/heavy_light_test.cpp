77f7a8af4f8fd5d4c0f535c4297ea53f26783097
Luis Santiago Re <lsantire2@gmail.com>
2023-10-16 03:32:36 +0100

    Update HLD and test

--- notebook/secciones/grafos/tests/heavy_light_test.cpp
+++ notebook/secciones/grafos/tests/heavy_light_test.cpp
@@ -24,81 +24,91 @@ typedef pair<int,int> ii;
 
 #define MAXN 10001
 
-//Dado un arreglo y una operacion asociativa con neutro
-#define operacion(x, y) max(x, y)
-typedef int tipo;
-const tipo neutro=0;
-struct RMQ {
+typedef ll tipo;
+const tipo neutro = 0;
+tipo oper(const tipo& a, const tipo& b) { return max(a,b); }
+struct ST {
 	int sz;
-	tipo t[4*MAXN];
-	tipo &operator[](int p) {return t[sz+p];}
-	void init(int n) {//O(nlgn)
+	vector<tipo> t;
+	ST(int n) {
 		sz = 1 << (32 - __builtin_clz(n));
-		forn(i, 2*sz) t[i] = neutro;
+		t = vector<tipo>(2*sz, neutro);
 	}
-	void updall() {dforn(i, sz) t[i]=operacion(t[2*i], t[2*i+1]);} //O(n)
-	tipo get(int i, int j) {return get(i,j,1,0,sz);}
-	tipo get(int i, int j, int n, int a, int b) {//O(lgn), [i, j)
-		if(j<=a || i>=b) return neutro;
-		if(i<=a && b<=j) return t[n];//n = node of range [a,b)
+	tipo &operator[](int p) { return t[sz+p]; }
+	void updall() { dforn(i, sz) t[i] = oper(t[2*i], t[2*i+1]); }
+	tipo get(int i, int j) { return get(i, j, 1, 0, sz); }
+	tipo get(int i, int j, int n, int a, int b) { //O(log n), [i, j)
+		if(j <= a || b <= i) return neutro;
+		if(i <= a && b <= j) return t[n]; // n = node of range [a,b)
 		int c = (a+b)/2;
-		return operacion(get(i, j, 2*n, a, c), get(i, j, 2*n+1, c, b));
+		return oper(get(i, j, 2*n, a, c), get(i, j, 2*n+1, c, b));
 	}
-	void set(int p, tipo val) {//O(lgn)
+	void set(int p, tipo val) { //O(log n)
 		p += sz;
-		while(p>0 && t[p]!=val) {
+		while(p>0 && t[p] != val) {
 			t[p] = val;
 			p /= 2;
-			val = operacion(t[p*2], t[p*2+1]);
+			val = oper(t[p*2], t[p*2+1]);
 		}
 	}
-}; //Use: definir operacion tipo neutro y MAXN,
-//cin >> n; rmq.init(n); forn(i, n) cin >> rmq[i]; rmq.updall();
+}; //Use: definir oper tipo neutro,
+//cin >> n; ST st(n); forn(i, n) cin >> st[i]; st.updall();
 
-vector<ii> g[MAXN];
-int wg[MAXN],dad[MAXN],dep[MAXN]; // weight,father,depth
-void dfs1(int x){
-	wg[x]=1;
-	for(ii y:g[x])if(y.fst!=dad[x]){
-		dad[y.fst]=x;dep[y.fst]=dep[x]+1;dfs1(y.fst);
-		wg[x]+=wg[y.fst];
+// Usage: 1. HLD(# nodes) 2. add tree edges 3. build() 4. use it
+struct HLD {
+	vector<int> w, p, dep; // weight,father,depth
+	vector<vector<int>> g;
+	HLD(int n) : w(n), p(n), dep(n), g(n), pos(n), head(n) {}
+	void addEdge(int a, int b) { g[a].pb(b); g[b].pb(a); }
+	void build() { p[0]=-1; dep[0]=0; dfs1(0); curpos=0; hld(0,-1); }
+	void dfs1(int x) {
+		w[x] = 1;
+		for(int y : g[x]) if(y != p[x]) {
+			p[y] = x; dep[y] = dep[x]+1; dfs1(y);
+			w[x] += w[y];
+		}
 	}
-}
-int curpos,pos[MAXN],head[MAXN];
-void hld(int x, int c){
-	if(c<0)c=x;
-	pos[x]=curpos++;head[x]=c;
-	int mx=-1;
-	for(ii y:g[x])if(y.fst!=dad[x]&&(mx<0||wg[mx]<wg[y.fst]))mx=y.fst;
-	if(mx>=0)hld(mx,c);
-	for(ii y:g[x])if(y.fst!=mx&&y.fst!=dad[x])hld(y.fst,-1);
-}
-void hld_init(){dad[0]=-1;dep[0]=0;dfs1(0);curpos=0;hld(0,-1);}
-int query(int x, int y, RMQ &st){
-	int r=neutro;
-	while(head[x]!=head[y]){
-		if(dep[head[x]]>dep[head[y]])swap(x,y);
-		r=operacion(r,st.get(pos[head[y]],pos[y]+1));
-		y=dad[head[y]];
+	int curpos;
+	vector<int> pos, head;
+	void hld(int x, int c) {
+		if(c < 0) c = x;
+		pos[x] = curpos++; head[x] = c;
+		int mx = -1;
+		for(int y : g[x]) if(y != p[x] && (mx < 0 || w[mx] < w[y])) mx = y;
+		if(mx >= 0) hld(mx, c);
+		for(int y : g[x]) if(y != mx && y != p[x]) hld(y, -1);
 	}
-	if(dep[x]>dep[y])swap(x,y); // now x is lca
-	r=operacion(r,st.get(pos[x]+1,pos[y]+1));
-	return r;
-}
-// for updating: rmq.upd(pos[x],v);
-// queries on edges: - assign values of edges to "child" node
-//                   - change pos[x] to pos[x]+1 in query (line 28)
+	// Here ST is segtree static/dynamic/lazy according to problem
+	tipo query(int x, int y, ST &st) { // ST tipo
+		tipo r = neutro;
+		while(head[x] != head[y]) {
+			if(dep[head[x]] > dep[head[y]]) swap(x,y);
+			r = oper(r, st.get(pos[head[y]], pos[y]+1)); // ST oper
+			y = p[head[y]];
+		}
+		if(dep[x] > dep[y]) swap(x,y); // now x is lca
+		r = oper(r, st.get(pos[x]+1, pos[y]+1)); // ST oper
+		return r;
+	}
+};
+// for point updates: st.set(pos[x], v) (x = node, v = new value)
+// for lazy range updates: something similar to the query method
+// for queries on edges: - assign values of edges to "child" node
+//                       - change pos[x] to pos[x]+1 in query (line 34)
 
-RMQ st;
+
+ST st(1);
+HLD hld(1);
 vector<int> edgeIdToNode;
 map<ii,int> edgeId;
+vector<vector<ii>> g;
 
 void buildSt(int node, int prev)
 {
 	forall(it,g[node]) if(it->fst != prev)
 	{
 		edgeIdToNode[edgeId[mp(node, it->fst)]] = it->fst;
-		st[pos[it->fst]] = it->snd;
+		st[hld.pos[it->fst]] = it->snd;
 		buildSt(it->fst, node);
 	}
 }
@@ -118,6 +128,8 @@ int main()
 	{
 		int n;
 		cin >> n;
+		hld = HLD(n);
+		g.rsz(n);
 		forn(i,n-1)
 		{
 			int a,b,c;
@@ -127,9 +139,10 @@ int main()
 			edgeId[mp(b,a)] = i;
 			g[a].pb(mp(b,c));
 			g[b].pb(mp(a,c));
+			hld.addEdge(a,b);
 		}
-		hld_init();
-		st.init(n);
+		hld.build();
+		st = ST(n);
 		edgeIdToNode = vector<int>(n-1);
 		buildSt(0,0);
 		st.updall();
@@ -142,19 +155,19 @@ int main()
 				int id,x;
 				cin >> id >> x;
 				id--;
-				st.set(pos[edgeIdToNode[id]], x);
+				st.set(hld.pos[edgeIdToNode[id]], x);
 			}
 			else
 			{
 				int a,b;
 				cin >> a >> b;
 				a--;b--;
-				cout << query(a,b,st) << '\n';
+				cout << hld.query(a,b,st) << '\n';
 			}
 		}
 		edgeId.clear();
 		edgeIdToNode.clear();
-		forn(i,n) g[i].clear();
+		g.clear();
 	}
 	return 0;
 }
