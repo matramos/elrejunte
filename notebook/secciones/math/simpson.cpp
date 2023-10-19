d89cc0eb5937696d1ba71ade104377960cb6226d
JU4N98 <juanpablocabana2@gmail.com>
2023-10-19 19:28:40 -0300

    changes method signature.
    Signed-off-by: JU4N98 <juanpablocabana2@gmail.com>

--- notebook/secciones/math/simpson.cpp
+++ notebook/secciones/math/simpson.cpp
@@ -1,7 +1,7 @@
 typedef long double T;
 
 // polar coordinates: x=r*cos(theta), y=r*sin(theta),  f=(r*r)/2
-T integral(T a, T b, int n=10000) { // O(n)
+T simpson(std::function<T(T)> f, T a, T b, int n=10000) { // O(n)
 	T area=0, h=(b-a)/T(n), fa=f(a), fb;
 	forn(i, n)
 	{
