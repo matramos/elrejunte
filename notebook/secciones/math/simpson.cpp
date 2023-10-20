e3eb58d6a5dbcc4501a4247541af8110b4208aa0
Juan Pablo Cabaña <46539158+JU4N98@users.noreply.github.com>
2023-10-19 21:08:24 -0300

    Simpson refactor (#39)
    * function is passed as parameter, cast literals, adds test.
    
    * changes method signature.
    
    Signed-off-by: JU4N98 <juanpablocabana2@gmail.com>
    
    ---------
    
    Signed-off-by: JU4N98 <juanpablocabana2@gmail.com>

--- notebook/secciones/math/simpson.cpp
+++ notebook/secciones/math/simpson.cpp
@@ -1,10 +1,12 @@
-double integral(double a, double b, int n=10000) //O(n), n=cantdiv
-{
-	double area=0, h=(b-a)/n, fa=f(a), fb;
+typedef long double T;
+
+// polar coordinates: x=r*cos(theta), y=r*sin(theta),  f=(r*r)/2
+T simpson(std::function<T(T)> f, T a, T b, int n=10000) { // O(n)
+	T area=0, h=(b-a)/T(n), fa=f(a), fb;
 	forn(i, n)
 	{
-		fb=f(a+h*(i+1));
-		area+=fa+ 4*f(a+h*(i+0.5)) +fb, fa=fb;
+		fb = f(a+h*T(i+1));
+		area += fa +T(4)*f(a+h*T(i+0.5)) +fb; fa=fb;
 	}
-	return area*h/6.;
+	return area*h/T(6.);
 }
