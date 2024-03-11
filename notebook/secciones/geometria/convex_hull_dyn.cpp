01c2ed367a13a736a6af8d48ca5b85235277b4ea
Luis Santiago Re <lsantire2@gmail.com>
2024-03-11 04:48:26 +0000

    Apply clang-format to folder 'geometria'
    Except for point, circle and poly

--- notebook/secciones/geometria/convex_hull_dyn.cpp
+++ notebook/secciones/geometria/convex_hull_dyn.cpp
@@ -1,41 +1,40 @@
 struct semi_chull {
-    set<pto> pt; // maintains semi chull without collinears points
-    // in case we want them on the set, make the changes commented below
-    bool check(pto p) {
-        if(pt.empty()) return false;
-        if(*pt.rbegin() < p) return false;
-        if(p < *pt.begin()) return false;
-        auto it = pt.lower_bound(p);
-        if(it->x == p.x) return p.y <= it->y; // remove for counting collinears
-        pto b = *it;
-        pto a = *prev(it);
-        return ((b-p)^(a-p)) + EPS >= 0; // change here for counting collinears
+  set<pto> pt;  // maintains semi chull without collinears points
+  // in case we want them on the set, make the changes commented below
+  bool check(pto p) {
+    if (pt.empty()) return false;
+    if (*pt.rbegin() < p) return false;
+    if (p < *pt.begin()) return false;
+    auto it = pt.lower_bound(p);
+    if (it->x == p.x) return p.y <= it->y;  // change? for collinears
+    pto b = *it;
+    pto a = *prev(it);
+    return ((b - p) ^ (a - p)) + EPS >= 0;  // change? for collinears
+  }
+  void add(pto p) {
+    if (check(p)) return;
+    pt.erase(p);
+    pt.insert(p);
+    auto it = pt.find(p);
+    while (true) {
+      if (next(it) == pt.end() || next(next(it)) == pt.end()) break;
+      pto a = *next(it), b = *next(next(it));
+      if (((b - a) ^ (p - a)) + EPS >= 0) {  // change? for collinears
+        pt.erase(next(it));
+      } else break;
     }
- 
-    void add(pto p) {
-        if(check(p)) return;
-        pt.erase(p); pt.insert(p);
-        auto it = pt.find(p);
-        while(true) {
-            if(next(it) == pt.end() || next(next(it)) == pt.end()) break;
-            pto a = *next(it), b = *next(next(it));
-            if(((b-a)^(p-a)) + EPS >= 0) {// change here for counting collinears
-                pt.erase(next(it));
-            } else break;
-        }
- 
-        it = pt.find(p);
-        while(true) {
-            if(it == pt.begin() || prev(it) == pt.begin()) break;
-            pto a = *prev(it), b = *prev(prev(it));
-            if(((b-a)^(p-a)) - EPS <= 0) {// change here for counting collinears
-                pt.erase(prev(it));
-            } else break;
-        }
+    it = pt.find(p);
+    while (true) {
+      if (it == pt.begin() || prev(it) == pt.begin()) break;
+      pto a = *prev(it), b = *prev(prev(it));
+      if (((b - a) ^ (p - a)) - EPS <= 0) {  // change? for collinears
+        pt.erase(prev(it));
+      } else break;
     }
+  }
 };
-struct CHD{
-    semi_chull sup, inf;
-    void add(pto p) { sup.add(p); inf.add(p*(-1)); }
-    bool check(pto p) { return sup.check(p) && inf.check(p*(-1)); }
+struct CHD {
+  semi_chull sup, inf;
+  void add(pto p) { sup.add(p), inf.add(p * (-1)); }
+  bool check(pto p) { return sup.check(p) && inf.check(p * (-1)); }
 };
--- notebook/secciones/geometria/radial_order.cpp
+++ notebook/secciones/geometria/radial_order.cpp
@@ -1,12 +1,12 @@
 // radial sort around point O in CCW direction starting from vector v
 struct cmp {
-	pto o, v;
-	cmp (pto no, pto nv) : o(no), v(nv) {}
-	bool half(pto p) {
-		assert(!(p.x == 0 && p.y == 0)); // (0,0) isn't well defined
-		return (v^p) < 0 || ((v^p) == 0 && (v*p) < 0); 
-	}
-	bool operator() (pto & p1, pto & p2) {
-		return mp(half(p1-o), T(0)) < mp(half(p2-o), ((p1-o)^(p2-o)));
-	}
+  pto o, v;
+  cmp(pto no, pto nv) : o(no), v(nv) {}
+  bool half(pto p) {
+    assert(!(p.x == 0 && p.y == 0));  // (0,0) isn't well defined
+    return (v ^ p) < 0 || ((v ^ p) == 0 && (v * p) < 0);
+  }
+  bool operator()(pto& p1, pto& p2) {
+    return mp(half(p1 - o), T(0)) < mp(half(p2 - o), ((p1 - o) ^ (p2 - o)));
+  }
 };
