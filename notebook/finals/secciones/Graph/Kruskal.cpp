cae81445931ae082b6feefc7e2e6a2b99c2a1097
JU4N98 <juanpablocabana2@gmail.com>
2024-09-03 16:46:30 -0300

    Adds kruskal.

---
+++ notebook/finals/secciones/Graph/Kruskal.cpp
@@ -0,0 +1,14 @@
+struct Edge {
+  int a, b, w;
+};
+bool operator<(const Edge& a, const Edge& b) { 
+    return a.w < b.w; 
+}
+// Minimun Spanning Tree in O(E log E)
+ll kruskal(vector<Edge> &E, int n) {
+  ll cost = 0; sort(E.begin(), E.end()); 
+  UnionFind uf(n);
+  forall(it, E) if(!uf.join(it->a, it->b))
+    cost += it->w;
+  return cost;
+}
