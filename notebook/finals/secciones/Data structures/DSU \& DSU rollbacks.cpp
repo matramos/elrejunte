15f65361e8e02794f04e5c24cc1f235700c8c8d6
JU4N98 <juanpablocabana2@gmail.com>
2024-09-03 16:29:53 -0300

    Merges DSU in a single file, adds kruskal.

--- notebook/finals/secciones/Data structures/DSU \& DSU rollbacks.cpp
+++ notebook/finals/secciones/Data structures/DSU \& DSU rollbacks.cpp
@@ -1,3 +1,19 @@
+struct UnionFind {
+  int nsets;
+  vector<int> f, setsz;  // f[i] = parent of node i
+  UnionFind(int n) : nsets(n), f(n, -1), setsz(n, 1) {}
+  int comp(int x){return (f[x]==-1 ? x : f[x]=comp(f[x]));}
+  bool join(int i, int j) {//returns true if already in same
+    int a = comp(i), b = comp(j);
+    if (a != b) {
+      if (setsz[a] > setsz[b]) swap(a, b);
+      f[a] = b; // big group (b) now represents small (a) 
+      nsets--, setsz[b] += setsz[a];
+    }
+    return a == b;
+  }
+};
+
 struct dsu_save {
   int v, rnkv, u, rnku;
   dsu_save() {}
