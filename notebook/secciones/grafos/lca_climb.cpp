dc4c86d83c3fd4c0af6022cea745dd7b1d8c9117
FedeNQ <fedenahuel07@gmail.com>
2024-12-21 15:48:19 -0300

    swap from vp[MAXN][LOG] to vp[LOG][MAXN] in lca_climb
    Signed-off-by: FedeNQ <fedenahuel07@gmail.com>

--- notebook/secciones/grafos/lca_climb.cpp
+++ notebook/secciones/grafos/lca_climb.cpp
@@ -2,36 +2,36 @@
 // Usage: 1) Create 2) Add edges 3) Call build 4) Use
 struct LCA {
   int N, LOGN, ROOT;
-  // vp[node][k] holds the 2^k ancestor of node
+  // vp[k][node] holds the 2^k ancestor of node
   // L[v] holds the level of v
   vector<int> L;
   vector<vector<int>> vp, G;
   LCA(int n, int root) : N(n), LOGN(lg(n) + 1), ROOT(root), L(n), G(n) {
     // Here you may want to replace the default from root to other
     // value, like maybe -1.
-    vp = vector<vector<int>>(n, vector<int>(LOGN, root));
+    vp = vector<vector<int>>(LOGN, vector<int>(n, root));
   }
   void addEdge(int a, int b) { G[a].pb(b), G[b].pb(a); }
   void dfs(int node, int p, int lvl) {
-    vp[node][0] = p, L[node] = lvl;
+    vp[0][node] = p, L[node] = lvl;
     forall(it, G[node]) if (*it != p) dfs(*it, node, lvl + 1);
   }
   void build() {
     // Here you may also want to change the 2nd param to -1
     dfs(ROOT, ROOT, 0);
-    forn(k, LOGN - 1) forn(i, N) vp[i][k + 1] = vp[vp[i][k]][k];
+    forn(k, LOGN - 1) forn(i, N) vp[k + 1][i] = vp[k][vp[k][i]];
   }
   int climb(int a, int d) {  // O(lgn)
     if (!d) return a;
-    dforn(i, lg(L[a]) + 1) if (1 << i <= d) a = vp[a][i], d -= 1 << i;
+    dforn(i, lg(L[a]) + 1) if (1 << i <= d) a = vp[i][a], d -= 1 << i;
     return a;
   }
   int lca(int a, int b) {  // O(lgn)
     if (L[a] < L[b]) swap(a, b);
     a = climb(a, L[a] - L[b]);
     if (a == b) return a;
-    dforn(i, lg(L[a]) + 1) if (vp[a][i] != vp[b][i]) a = vp[a][i], b = vp[b][i];
-    return vp[a][0];
+    dforn(i, lg(L[a]) + 1) if (vp[i][a] != vp[i][b]) a = vp[i][a], b = vp[i][b];
+    return vp[0][a];
   }
   int dist(int a, int b) {  // returns distance between nodes
     return L[a] + L[b] - 2 * L[lca(a, b)];
